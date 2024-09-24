1cbac5c94d9d51d95579d6cc064fcddf2237db84
ENZO GONCALVES SANTOS <enzo.gsantos@senacsp.edu.br>
2024-09-24 15:02:34 -0300

    primeiro commit

---
+++ exercicio/script.js
@@ -0,0 +1,18 @@
+
+
+const nome = prompt("Digite seu nome");
+const idade = prompt("Digite sua idade");
+let sub = 100 - idade ;
+alert(`Bem vindo(a), ${nome}, você tem ${idade} anos de idade, falta ${sub} anos para 100 anos! `);
+
+//Feito pelo professor
+/*
+let nome = prompt("Insira seu nome: ");
+
+let idade = parseInt(prompt("Insira sua idade"))
+
+let anosParaCem = 100 - idade
+
+alert(`${nome}, faltam ${anosParaCem} para você completar 100 anos!`)
+
+*/
